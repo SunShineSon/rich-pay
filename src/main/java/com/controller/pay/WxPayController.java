24bf753157a79b6bbea962fdfdb4a841e527f090
SunShineSon <sunshineguanlan@gmail.com>
2018-02-06 23:14:40 +0800

    1、添加

---
+++ src/main/java/com/controller/pay/WxPayController.java
@@ -0,0 +1,9 @@
+package com.controller.pay;
+
+public class WxPayController {
+
+	public static void main(String[] args) {
+
+	}
+
+}
--- src/main/java/com/controller/user/UserController.java
+++ src/main/java/com/controller/user/UserController.java
@@ -2,8 +2,16 @@ package com.controller.user;
 
 public class UserController {
 
+	
 	public static void main(String[] args) {
 
 	}
 
+	public void login(){
+		
+	}
+	
+	public void loginOut(){
+		
+	}
 }
