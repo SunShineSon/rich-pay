1b0fee63d8cb48b792c230b29b43b94a96a0ffb3
hua <hua@hua-PC>
2018-02-10 14:14:34 +0800

    1、注解模式

--- src/main/java/com/controller/pay/WxPayController.java
+++ src/main/java/com/controller/pay/WxPayController.java
@@ -1,9 +1,40 @@
 package com.controller.pay;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * 
+ * 2018年2月9日 下午5:42:50
+ * 
+ * @Author：郭 
+ * @Description： 微信支付接口
+ *
+ */
+@Controller
+@RequestMapping("/wxPay")
 public class WxPayController {
 
 	public static void main(String[] args) {
 
+	}
+
+	/**
+	 * 
+	 * 2018年2月9日 下午5:47:33
+	 * @Author：郭
+	 * @param req
+	 * @param res
+	 * @param code
+	 * @Description：
+	 */
+	@RequestMapping(value = "/pay", method = { RequestMethod.POST, RequestMethod.GET })
+	public void pay(HttpServletRequest req, HttpServletResponse res, String code) {
+		System.out.println("注释接口");
 	}
 
 }
